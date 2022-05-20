f969feb3c4660fd68f8e150149f9221007eab35f
morgan974 <oconnormorgan974@gmail.com>
2022-05-20 09:59:34 +0400

    Rush de fin

--- src/components/Elements/CalendarElement.tsx
+++ src/components/Elements/CalendarElement.tsx
@@ -1,35 +1,81 @@
-import React, {useState} from 'react';
-import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
+import React, {useEffect, useState} from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid'
+import axios from "axios";
+import {AddressApi} from "../../config/CommonConst";
+import interactionPlugin from "@fullcalendar/interaction"
+import moment from "moment";
+import ModalElement from "./ModalElement";
+import AddTrekFeature from "../Feature/AddTrekFeature";
 
-interface Props {}
+interface Props {
+    idTrek: string;
+}
 
-const CalendarElement : React.ComponentType<Props> = (
-    {}) => {
-    const [value, onChange] = useState(new Date());
+const CalendarElement: React.ComponentType<Props> = ({
+    idTrek
+ }) => {
 
-    const dates = [
-        new Date(2022, 4, 28),
-        new Date(2022, 4, 20),
-        new Date(2022, 4, 25),
-        new Date(2022, 4, 23)
-    ];
+    const [datas, setDatas] = useState([]);
+    const [formatDatas, setFormatDatas] = useState({});
+    const [error, setError] = useState('');
 
-    const handleBooking = (event : any) => {
-        console.log(event);
-    }
+    useEffect(() => {
+        getDatas();
+    }, []);
+
+    const handleDateClick = (event : any) => {
+        console.log(idTrek);
+        axios
+            .post(AddressApi + "/api/books", {date: event.dateStr, trek: idTrek})
+            .then(response => {
+                setDatas(response.data);
+            })
+            .catch(error => setError("Vous devez vous connecter !"))
+        ;
+    };
+
+    const getDatas = () => {
+        axios
+            .get(AddressApi + "/api/books")
+            .then(response => {
+                setDatas(response.data);
+            })
+            .catch(error => setError("Error"))
+        ;
+    };
+
+    useEffect(() => {
+        if(datas) {
+            let tmpDatas : any = [];
+            if(datas.length > 0) {
+                datas.forEach((e:any) => {
+                    tmpDatas = [
+                        ...tmpDatas,
+                        {
+                            title: e?.trek?.name,
+                            date: moment(e?.date).format('YYYY-MM-DD')
+                        }
+                    ];
+                })
+            }
+            setFormatDatas(tmpDatas);
+        }
+    }, [datas]);
 
     return (
-        <Calendar
-            onChange={onChange}
-            defaultValue={dates}
-            defaultActiveStartDate={new Date()}
-            showNeighboringMonth
-            selectRange={false}
-            onClickDay={handleBooking}
-            minDate={new Date()}
-        />
-    );
+        <div className="calendar">
+            { error &&
+                <p className="text-error">{error}</p>
+            }
+            <FullCalendar
+                plugins={[ dayGridPlugin, interactionPlugin ]}
+                initialView="dayGridMonth"
+                events={formatDatas}
+                dateClick={handleDateClick}
+            />
+        </div>
+    )
 }
 
 export default CalendarElement;
